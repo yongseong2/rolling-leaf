c9965d567c4dbbb357cd076fe0570c603acb1824
yongseong2 <ancjs369@naver.com>
2024-03-27 21:58:06 +0900

    Fix: 빌드에러 해결

--- app/(login)/_components/LandingBackground.tsx
+++ app/(login)/_components/LandingBackground.tsx
@@ -1,8 +1,8 @@
 export function LandingBackground() {
   return (
     <>
-      <div className="from-c2 to-c1 absolute inset-0 z-0 bg-radial-at-c" />
-      <div className="from-c1 to-c2 absolute -bottom-52 z-0 h-9/10 w-full flex-1 rounded-full bg-linear-custom-gradient" />
+      <div className="absolute inset-0 z-0 bg-radial-at-c from-c2 to-c1" />
+      <div className="absolute -bottom-52 z-0 h-9/10 w-full flex-1 rounded-full bg-linear-custom-gradient from-c1 to-c2" />
     </>
   );
 }
