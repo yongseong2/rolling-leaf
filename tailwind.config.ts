ea29c3a1304d3c00ceb956701cbc047c83038762
yongseong2 <ancjs369@naver.com>
2024-03-30 11:23:54 +0900

    Feat: 초기설정 및 라이브러리 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,12 @@ const config: Config = {
         action: "#00916e",
         "dark-text": "#002b20",
         gray: "#888888",
+        "light-cream": "#f3e9d2", // 배경색이나 부드러운 분위기
+        "deep-green": "#004d47", // 강조 요소나 버튼
+        "soft-sand": "#dcd1c5", // 텍스트 배경이나 구분선
+        "pale-blue": "#a7cdd8", // 활기찬 느낌을 위해
+        "deep-gray": "#565656", // 텍스트나 중요 요소 강조
+        "wine-red": "#722f37", // 주의를 끌고 싶은 버튼이나 호출 액션
       },
       height: {
         "9/10": "110%",
@@ -29,6 +35,15 @@ const config: Config = {
       boxShadow: {
         "top-shadow": "0 -1px 1px -1px rgba(0, 0, 0, 0.25)",
         "bottom-shadow": "0 1px 1px -1px rgba(0, 0, 0, 0.25)",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-20px)" },
+        },
+      },
+      animation: {
+        float: "float 3s ease-in-out infinite",
       },
     },
   },
