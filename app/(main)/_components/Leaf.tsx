f56a090246a20a471f007731fcd99909e6c7e442
yongseong2 <ancjs369@naver.com>
2024-03-30 13:45:54 +0900

    Feat: 연못안에 나뭇잎이 있게끔 로직 수정

--- app/(main)/_components/Leaf.tsx
+++ app/(main)/_components/Leaf.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Draggable, { DraggableEventHandler } from "react-draggable";
 
 interface Props {
@@ -8,21 +8,37 @@ interface Props {
     y: number;
     title: string;
   };
+
   onDrag: DraggableEventHandler;
 }
 
 export function Leaf({ leaf, onDrag }: Props) {
   const nodeRef = useRef(null);
+  const [opacity, setOpacity] = useState(false);
+  const handleStart = () => {
+    setOpacity(true);
+  };
+  const handleStop = () => {
+    setOpacity(false);
+  };
+  const randomPosition = (max: number) => Math.floor(Math.random() * max);
+
   return (
     <Draggable
       onDrag={onDrag}
+      onStart={handleStart}
+      onStop={handleStop}
       position={leaf}
       bounds="parent"
       nodeRef={nodeRef}
     >
-      <div className="relative z-10 h-28 w-20 cursor-move" ref={nodeRef}>
+      <div
+        className={`relative z-10 h-28 w-20 cursor-move ${opacity ? "opacity-60" : ""}`}
+        ref={nodeRef}
+        style={{ position: "absolute" }}
+      >
         <p className="absolute -right-3 -top-3 z-20 text-c0">{leaf.title}</p>
-        <div className="h-full w-full animate-float bg-leaf bg-cover" />
+        <div className="size-full animate-float bg-leaf bg-cover" />
       </div>
     </Draggable>
   );
