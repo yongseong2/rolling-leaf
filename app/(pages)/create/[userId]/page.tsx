d3761725aff1c2292b71412b191af71ef6116037
yongseong2 <ancjs369@naver.com>
2024-04-01 11:21:13 +0900

    Feat: 생성페이지 라우팅

---
+++ app/(pages)/create/[userId]/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function CreatePage({ params }: { params: { userId: string } }) {
+  return <div>생성페이지</div>;
+}
--- app/_routes/index.ts
+++ app/_routes/index.ts
@@ -1,4 +1,5 @@
 export const routes = {
   home: "/home",
   login: "/login",
+  create: "/create",
 };
