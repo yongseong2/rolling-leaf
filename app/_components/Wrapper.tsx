a22a1c7405605322f445bdff22422fff0ca24e27
yongseong2 <ancjs369@naver.com>
2024-04-02 18:47:00 +0900

    Feat: 뒤로가기 버튼 생성

--- app/_components/Wrapper.tsx
+++ app/_components/Wrapper.tsx
@@ -4,6 +4,7 @@ import IconButton from "./IconButton";
 import { colors } from "@/app/_design/colors";
 import { useParams, usePathname, useRouter } from "next/navigation";
 import { routes } from "../_routes";
+import Icon from "./Icon";
 
 interface Props {
   children: ReactNode;
@@ -17,17 +18,28 @@ export const Wrapper = ({ children }: Props) => {
   const params = useParams();
 
   return (
-    <main className="relative flex h-screen min-h-screen max-w-screen-md flex-col overflow-auto bg-linear-custom-gradient from-c1 to-c2 px-6 py-14">
-      {isHome && (
-        <IconButton
-          className="absolute right-5 top-5 flex size-10 items-center justify-center rounded-full bg-c2"
-          name="Plus"
-          color={colors.c0}
-          onClick={() =>
-            router.push(`${routes["select-leaf"]}/${params.userId}`)
-          }
-        />
-      )}
+    <main className="relative flex h-screen min-h-screen max-w-screen-md flex-col overflow-auto bg-linear-custom-gradient from-c1 to-c2 px-6 pb-7">
+      <header className="w-full py-3">
+        {!isHome && (
+          <button
+            onClick={() => router.back()}
+            className="flex items-center justify-center"
+          >
+            <Icon size={20} name="Back" color={colors.c0} />
+            <p>뒤로가기</p>
+          </button>
+        )}
+        {isHome && (
+          <IconButton
+            className="flex size-10 items-center justify-center rounded-full bg-c2"
+            name="Plus"
+            color={colors.c0}
+            onClick={() =>
+              router.push(`${routes["select-leaf"]}/${params.userId}`)
+            }
+          />
+        )}
+      </header>
       {children}
     </main>
   );
--- app/_design/colors.ts
+++ app/_design/colors.ts
@@ -5,4 +5,10 @@ export const colors = {
   action: "#00916e",
   "dark-text": "#002b20",
   gray: "#888888",
+  "light-cream": "#f3e9d2", // 배경색이나 부드러운 분위기
+  "deep-green": "#004d47", // 강조 요소나 버튼
+  "soft-sand": "#dcd1c5", // 텍스트 배경이나 구분선
+  "pale-blue": "#a7cdd8", // 활기찬 느낌을 위해
+  "deep-gray": "#565656", // 텍스트나 중요 요소 강조
+  "wine-red": "#722f37", // 주의를 끌고 싶은 버튼이나 호출 액션
 };
