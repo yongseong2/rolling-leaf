2051f35fccf3f5f8bbc73134e301aaad89cd8d4a
yongseong2 <ancjs369@naver.com>
2024-04-03 21:41:36 +0900

    Feat: 카카오로그인 구현

--- app/(pages)/api/auth/[...nextauth]/route.ts
+++ app/(pages)/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,27 @@
-import { handlers } from "@/auth";
+import NextAuth from "next-auth/next";
+import KakaoProvider from "next-auth/providers/kakao";
 
-export const { GET, POST } = handlers;
+const handler = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+
+    async session({ session, token }) {
+      session.user = token;
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export { handler as GET, handler as POST };
