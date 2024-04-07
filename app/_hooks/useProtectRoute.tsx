6d1845ae2242d5d31145edba2f66d26b278d2ef3
yongseong2 <ancjs369@naver.com>
2024-04-07 09:17:36 +0900

    Fix: ProtectedRoute 안정화

--- app/_hooks/useProtectRoute.tsx
+++ app/_hooks/useProtectRoute.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export function useProtectRoute() {
   const session = useSession();
   const router = useRouter();
+  const pathname = usePathname();
+  const isLoginPage = pathname === "/login" || pathname === "/oauth";
+
   useEffect(() => {
-    if (session.status === "authenticated") {
+    if (session.status === "authenticated" && isLoginPage) {
       router.push(`/home/${session.data.user?.id}`);
-    } else if (session.status === "unauthenticated") {
+    } else if (session.status === "unauthenticated" && !isLoginPage) {
       router.push("/login");
     }
-  }, [session.status, router]);
+  }, [session.status, router, isLoginPage]);
 }
